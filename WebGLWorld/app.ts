1af2a33e7da8dba7ab20335c68297cff4b709695
Francisco Sandoval <franciscosandoval15@gmail.com>
2012-12-01 00:14:09 -0300

    ultima modificacion con escena importada mal

--- WebGLWorld/app.ts
+++ WebGLWorld/app.ts
@@ -8,7 +8,6 @@ $(document).ready(function() {
     world = new World();
 });
 
-
 /// Clase que contiene todos los elementos de WebGL + ThreeJS
 class World {
     private canvasWidth = window.innerWidth;
@@ -24,62 +23,46 @@ class World {
     constructor() {
         this.camera = new THREE.PerspectiveCamera(75, this.canvasWidth/this.canvasHeight, 1, 10000);
         this.camera.position.z = 600;
-    
-        //----------------------------cubo --------------------------------
-        this.scene = new THREE.Scene(); 
-        var geometry = new THREE.CubeGeometry(200, 200, 200); 
-        var material = new THREE.MeshBasicMaterial({ color: 0xcc0000, wireframe: false });
 
-        this.mesh = new THREE.Mesh(geometry, material);
-        //this.scene.add(this.mesh);
-        //---------------------end cubo -------------------------------
-
-        //-------------------- terreno -------------------------------
-        // GROUND
-	/*
-        	var groundGeo = new THREE.PlaneGeometry( 10000, 10000 );
-		var groundMat = new THREE.MeshPhongMaterial( { ambient: 0xffffff, color: 0xffffff, specular: 0x050505 } );
-		groundMat.color.setHSV( 0.095, 0.5, 1 );
-
-		var ground = new THREE.Mesh( groundGeo, groundMat );
-		ground.rotation.x = -Math.PI/2;
-		ground.position.y = -33;
-		this.scene.add( ground );
-
-		ground.receiveShadow = true;
-    */
-        // --------------------------------------------------------
-
-        //-------------- lights -------------------------
-		var light = new THREE.DirectionalLight(0x111111, 100);
-		light.position.x = 20;
-		light.position.y = 20;
-		this.scene.add(light);
-        //-------------- end lights -------------------------
-        
-        this.controls = new THREE.FirstPersonControls( this.camera );
-
-		this.controls.movementSpeed = 60;
-		this.controls.lookSpeed = 0.05;
-		this.controls.noFly = true;
-		this.controls.lookVertical = false;
-        
         this.renderer = new THREE.WebGLRenderer();
         this.renderer.setSize(this.canvasWidth, this.canvasHeight);
         $('#canvas-wrapper').append($(this.renderer.domElement));
 
-        this.boids = new Boids();
-        this.boids.loadModel(this.scene, () => {
-            this.animate();
-        });
+        var loader = new THREE.SceneLoader();
+
+		loader.load( "models/scene/our_scene.js", ( loaded ) => {
+            this.camera = loaded.currentCamera;
+			this.camera.updateProjectionMatrix();
+            this.scene = loaded.scene;
+            this.renderer.setClearColor( loaded.bgColor, loaded.bgAlpha );
+
+            //------------------------------- lights -------------------------------
+		    var light = new THREE.DirectionalLight(0x111111, 100);
+		    light.position.x = 20;
+		    light.position.y = 20;
+		    this.scene.add(light);
+            //------------------------------- end lights -------------------------------
+        
+            //------------------------------- controles -------------------------------
+            this.controls = new THREE.FirstPersonControls( this.camera );
+		    this.controls.movementSpeed = 60;
+		    this.controls.lookSpeed = 0.02;
+		    this.controls.noFly = true;
+		    this.controls.lookVertical = true;
+            //------------------------------- end controles -------------------------------
+
+            //------------------------------- BOIDS -------------------------------
+            this.boids = new Boids();
+            this.boids.loadModel(this.scene, () => {
+                this.animate();
+            });
+            //------------------------------- end boids -------------------------------        
+      });
     }
 
     animate() {
         // Prepararse para dibujar siguiente frame:
         requestAnimationFrame(() => this.animate());
-
-        this.mesh.rotation.x += 0.01;
-        this.mesh.rotation.y += 0.02;
         
         var delta = this.clock.getDelta(),
 		time = this.clock.getElapsedTime() * 5;
